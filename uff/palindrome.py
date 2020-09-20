77cd4855a18c16fb806fafc7dfd033cd7f5b51b9
joaog314 <57019628+joaog314@users.noreply.github.com>
2020-09-20 00:19:40 -0300

    uff tired job

--- uff/palindrome.py
+++ uff/palindrome.py
@@ -1,29 +1,47 @@
 def palind_ver(word):
-    letters = entry.split()
-    list_str = list(','.join(letters))
-    n = len(list_str) 
-    p = 0
 
+    letters = word.split() # separação de letras para criação de uma lista.
+
+    list_str = list(','.join(letters)) # criação de lista com as letras da palavra a ser avaliada.
+
+    n = len(list_str) # variável com o tamanho da palavra a ser iterada.
+
+    p = 0 # contador de iterações com resultado verdadeiro.
+
+    # iteração para palíndrome pura.
     for i in range(n//2):
         if (list_str[i] == list_str[n-i-1]) == True:
             p = p + 1
     if p == n//2:
         return('palíndrome pura')
 
+    p = 0
+    # iteração para palíndrome quebrada.
     for i in range(n//2):
-        if (list_str[i] < list_str[n-i-1]) == True:
+        if (
+                ((list_str[i] < list_str[i+1]) and (list_str[n-i-2] > list_str[n-i-1])) == True 
+            
+                or ((list_str[i] > list_str[i+1]) and (list_str[n-i-2] < list_str[n-i-1])) == True
+            
+                or ((list_str[i] == list_str[i+1]) and (list_str[n-i-2] == list_str[n-i-1])) == True
+            ):
             p = p + 1
+
     if p == n//2:
-        return('palíndrome quebrada')
+        return( 'palíndrome quebrada')
     else:
-        return('não é palíndrome')
+        return('não é uma palíndrome')
 
 start = 1
 while start == 1:
     word = str(input('Digite uma palavra, ou s para sair: ')).upper()
     if word == 'S':
+        print('fim do programa...')
         start = 0
     elif len(word) >= 3:
         print(palind_ver(word))
     else:
         print('palavra curta demais')
+
+# AAAEAEAEAAA
+# SUBINOONIBUS
--- uff/test.py
+++ uff/test.py
@@ -1,1 +1,48 @@
-strip() 
+word = 'SUBINOONIBUS'
+
+letters = word.split() # separação de letras para criação de uma lista.
+
+list_str = list(','.join(letters)) # criação de lista com as letras da palavra a ser avaliada.
+
+n = len(list_str) # variável com o tamanho da palavra a ser iterada.
+
+result = 0 # varíavel para avaliação de resultados das iterações.
+
+p = 0 # contador de iterações com resultado verdadeiro.
+
+# iteração para palíndrome pura.
+for i in range(n//2):
+    if (list_str[i] == list_str[n-i-1]) == True:
+        p = p + 1
+if p == n//2:
+    result = result + 1
+
+p = 0
+# iteração para palíndrome quebrada.
+for i in range(n//2):
+    if (
+            ((list_str[i] < list_str[i+1]) and (list_str[n-i-2] > list_str[n-i-1])) == True 
+        
+            or ((list_str[i] > list_str[i+1]) and (list_str[n-i-2] < list_str[n-i-1])) == True
+        
+            or ((list_str[i] == list_str[i+1]) and (list_str[n-i-2] == list_str[n-i-1])) == True
+        ):
+        p = p + 1
+
+if p == n//2:
+    result = result + 2
+
+if result == 1: 
+    print('palíndrome pura')
+elif result == 2:
+    print( 'palíndrome quebrada')
+elif result == 3:
+    print( 'palíndrome pura & quebrada')
+else:
+    print( 'não é uma palíndrome')
+
+# BIT
+# AAAEAEAEAAA
+# SUBINOONIBUS
+# TIPOERROTUDO
+# ERAREHUMANO
