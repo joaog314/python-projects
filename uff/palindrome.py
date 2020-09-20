427a38839de19558a884f1e8ac3b1d6772e852b0
joaog314 <57019628+joaog314@users.noreply.github.com>
2020-09-20 11:55:37 -0300

    indent adjust

--- uff/palindrome.py
+++ uff/palindrome.py
@@ -1,16 +1,18 @@
 def palind_ver(word):
 
-    letters = word.split() # separação de letras para criação de uma lista.
+    # separação de letras para criação de uma lista.
+    letters = word.split()
+    # criação de lista com as letras da palavra a ser avaliada.
+    list_str = list(','.join(letters))
+    # variável com o tamanho da palavra a ser iterada.
+    n = len(list_str)
 
-    list_str = list(','.join(letters)) # criação de lista com as letras da palavra a ser avaliada.
-
-    n = len(list_str) # variável com o tamanho da palavra a ser iterada.
-
-    p = 0 # contador de iterações com resultado verdadeiro.
+    # contador de iterações com resultado verdadeiro.
+    p = 0
 
     # iteração para palíndrome pura.
     for i in range(n//2):
-        if (list_str[i] == list_str[n-i-1]) == True:
+        if (list_str[i] == list_str[n-i-1]) is True:
             p = p + 1
     if p == n//2:
         return('palíndrome pura')
@@ -19,18 +21,30 @@ def palind_ver(word):
     # iteração para palíndrome quebrada.
     for i in range(n//2):
         if (
-                ((list_str[i] < list_str[i+1]) and (list_str[n-i-2] > list_str[n-i-1])) == True 
-           
-                or ((list_str[i] > list_str[i+1]) and (list_str[n-i-2] < list_str[n-i-1])) == True
-           
-                or ((list_str[i] == list_str[i+1]) and (list_str[n-i-2] == list_str[n-i-1])) == True
+                (
+                    (list_str[i] < list_str[i+1])
+                    and (list_str[n-i-2] > list_str[n-i-1])
+                ) is True
+
+                or
+                (
+                    (list_str[i] > list_str[i+1])
+                    and (list_str[n-i-2] < list_str[n-i-1])
+                ) is True
+
+                or
+                (
+                    (list_str[i] == list_str[i+1])
+                    and (list_str[n-i-2] == list_str[n-i-1])
+                ) is True
            ):
             p = p + 1
 
     if p == n//2:
-        return( 'palíndrome quebrada')
+        return('palíndrome quebrada')
     else:
         return('não é uma palíndrome')
+
 
 start = 1
 while start == 1:
@@ -64,4 +78,4 @@ while start == 1:
 # (B) (iv) e (v) são palíndromes quebradas
 # (C) (ii) não é palíndrome pura.
 # (D) (iii) é palíndrome pura e (iv) é palíndrome quebrada.
-# (E) (v) é palíndrome quebrada.
+# (E) (v) é palíndrome quebrada
