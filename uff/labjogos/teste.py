6058891af99d3bc3f42e72a3726a0b17aef328ae
João Gabriel Silva <57019628+joaog314@users.noreply.github.com>
2021-02-27 09:48:02 -0300

    cleaning

--- uff/labjogos/teste.py
+++ uff/labjogos/teste.py
@@ -1,10 +1,1 @@
-from PPlay.window import *
-from PPlay.gameimage import *
-from PPlay.sprite import *
-
-janela = Window(600,600)
-background = GameImage("fundo.jpg")
-
-while (True):
-    background.draw()
-    janela.update()
+    
